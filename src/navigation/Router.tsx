d744c4ce5502c9f0188fd611cc1b2cb51908804c
bilalseen <bilalsen2010@gmail.com>
2024-12-11 21:41:05 +0300

    feat: add NoteList screen to Router for navigation

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Timer from "../screens/Timer";
 import { NavigationContainer } from "@react-navigation/native";
+import NoteList from "../screens/notes/NoteList";
 
 const Stack = createNativeStackNavigator();
 
@@ -8,11 +9,13 @@ const Router = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
+        initialRouteName="NoteList"
         screenOptions={{
           headerShown: false,
         }}
       >
         <Stack.Screen name="Timer" component={Timer} />
+        <Stack.Screen name="NoteList" component={NoteList} />
       </Stack.Navigator>
     </NavigationContainer>
   );
