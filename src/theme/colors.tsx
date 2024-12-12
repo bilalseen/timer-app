a7e9583ae3db0e9e22d3f828dbb198656667da6a
bilalseen <bilalsen2010@gmail.com>
2024-12-12 12:07:13 +0300

    feat: add color theme configuration

---
+++ src/theme/colors.tsx
@@ -0,0 +1,10 @@
+const Colors = {
+  primary: "#FE6902", // Main accent color
+  background: "#010101", // Main background color
+  surface: "#1B1B1B", // Cards and panels
+  textPrimary: "#E5E5E5", // Primary text
+  textSecondary: "#393737", // Secondary text
+  border: "#262626", // Lines and borders
+};
+
+export default Colors;
