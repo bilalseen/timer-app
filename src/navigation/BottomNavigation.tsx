fe7bdcb5076bf5938aee790c7ce7e7285bb264d0
bilalseen <bilalsen2010@gmail.com>
2024-12-12 11:07:02 +0300

    feat: add BottomNavigation component with tabs for Notes and Timora

---
+++ src/navigation/BottomNavigation.tsx
@@ -0,0 +1,50 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+import Timora from "../screens/Timora";
+import NoteList from "../screens/notes/NoteList";
+
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+const BottomNavigation = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          marginHorizontal: 20,
+          bottom: 20,
+          borderRadius: 10,
+          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Notes"
+        component={NoteList}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="notebook" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Timora"
+        component={Timora}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="clock-digital"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomNavigation;
