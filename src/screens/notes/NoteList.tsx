f6ba0dbea95d019b692b7500ca187ae067342e9a
bilalseen <bilalsen2010@gmail.com>
2024-12-12 12:34:21 +0300

    refactor: remove unused import of CategoryCard from NoteList screen

--- src/screens/notes/NoteList.tsx
+++ src/screens/notes/NoteList.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Header from "../../components/Header";
 import { useFonts } from "expo-font";
 import Colors from "../../theme/colors";
-import CategoryCard from "../../components/CategoryCard";
 import CategoryList from "../../components/CategoryList";
 
 const NoteList = () => {
