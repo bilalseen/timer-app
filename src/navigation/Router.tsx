11c97fc5f839e7736638d1060f95688b45a24a10
bilalseen <bilalsen2010@gmail.com>
2024-12-12 18:18:48 +0300

    feat: wrap NavigationContainer with Redux Provider for state management

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -2,19 +2,23 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { NavigationContainer } from "@react-navigation/native";
 import BottomNavigation from "./BottomNavigation";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="BottomNavigation" component={BottomNavigation} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="BottomNavigation" component={BottomNavigation} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
