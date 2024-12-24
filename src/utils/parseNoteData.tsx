2809fab2f8ff57d60eda8e796e31bd871524a397
bilalseen <bilalsen2010@gmail.com>
2024-12-24 08:51:30 +0300

    feat: add parseNoteData utility for sorting notes by date

---
+++ src/utils/parseNoteData.tsx
@@ -0,0 +1,16 @@
+interface Note {
+  id: string;
+  title: string;
+  content: string;
+  categories: string[];
+  lastCategories: string[];
+  date: string;
+}
+
+export default function parseNoteData(notes: Note[]) {
+  const filteredNotes = [...notes].sort((a, b) => {
+    return a.date > b.date ? -1 : a.date < b.date ? 1 : 0;
+  });
+
+  return filteredNotes;
+}
