42f5000e5633758df3a700902508ca2f81625407
bilalseen <bilalsen2010@gmail.com>
2024-12-26 21:44:13 +0300

    feat: add color theme for Todo application

---
+++ src/theme/todo/colors.tsx
@@ -0,0 +1,11 @@
+const todoColors = {
+  primary: "#9E78CF", // Progress bar, buttons, and highlight texts.
+  background: "#0D0714", // General background.
+  textPrimary: "#9E78CF", // Main text, content guiding the user.
+  textSecondary: "#FFFFFF", // Less prioritized text (e.g., completed tasks).
+  completedTaskLine: "#78CFB0", // For strikethrough tasks.
+  buttonBackground: "#9E78CF", // Buttons like "Add Task".
+  highlight: "#805AD5", // Fine details and highlight elements.
+};
+
+export default todoColors;
