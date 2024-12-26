8ba99db8dd39dbbfd9f281eba1419df7c667b707
bilalseen <bilalsen2010@gmail.com>
2024-12-26 23:45:57 +0300

    feat: update color theme for Todo application with new container background and adjusted completed task line color

--- src/theme/todo/colors.tsx
+++ src/theme/todo/colors.tsx
@@ -1,9 +1,10 @@
 const todoColors = {
   primary: "#9E78CF", // Progress bar, buttons, and highlight texts.
   background: "#0D0714", // General background.
+  todoContainerBackground: "#1D1825", // Background for the todo container.
   textPrimary: "#9E78CF", // Main text, content guiding the user.
   textSecondary: "#FFFFFF", // Less prioritized text (e.g., completed tasks).
-  completedTaskLine: "#78CFB0", // For strikethrough tasks.
+  completedTaskLine: "#0D0714", // For strikethrough tasks.
   buttonBackground: "#9E78CF", // Buttons like "Add Task".
   highlight: "#805AD5", // Fine details and highlight elements.
 };
