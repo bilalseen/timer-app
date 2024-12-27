61345e053b77fe3d97ae34fa5501e7c12c57b58a
bilalseen <bilalsen2010@gmail.com>
2024-12-27 10:39:50 +0300

    feat: update Todo screen to display uncompleted tasks using FlatList

--- src/screens/todo/Todo.tsx
+++ src/screens/todo/Todo.tsx
@@ -1,10 +1,32 @@
-import { View, Text, StyleSheet, StatusBar } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, StatusBar, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
 import todoColors from "../../theme/todo/colors";
 import { AnimatedCircularProgress } from "react-native-circular-progress";
 import Card from "../../components/todo/Card";
+import { useSelector } from "react-redux";
+import { selectCompleted, selectUncompleted } from "../../redux/todoSlice";
+
+interface Todo {
+  id: string;
+  title: string;
+  content: string;
+  date: string;
+  isEdited: boolean;
+  completed: boolean;
+}
 
 const Todo = () => {
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
+  const [unCompletedTodos, setUnCompletedTodos] = useState<Todo[]>([]);
+
+  const unComplete = useSelector(selectUncompleted);
+  const completed = useSelector(selectCompleted);
+
+  useEffect(() => {
+    setCompletedTodos(completed);
+    setUnCompletedTodos(unComplete);
+  }, [unComplete, completed]);
+
   return (
     <View style={styles.container}>
       <StatusBar
@@ -34,7 +56,12 @@ const Todo = () => {
       </AnimatedCircularProgress>
       <Text style={styles.firstTitleText}>Your're doing great,</Text>
       <Text style={styles.secondTitleText}>your're halfway there!</Text>
-      <Card />
+      <FlatList
+        data={unCompletedTodos}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Card item={item} />}
+        ListFooterComponent={() => <View style={{ height: 70 }} />}
+      />
     </View>
   );
 };
