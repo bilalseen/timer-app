0d3dc9528771619eafdf6b3b601b22e46d1cbcd7
bilalseen <bilalsen2010@gmail.com>
2024-12-30 17:57:12 +0300

    feat: initialize todos and completed states with sample data in redux slice

--- src/redux/todoSlice.tsx
+++ src/redux/todoSlice.tsx
@@ -23,8 +23,66 @@ interface TodosState {
 }
 
 const initialState: TodosState = {
-  todos: [],
-  completed: [],
+  todos: [
+    {
+      id: "1",
+      title: "First todo",
+      content: "This is the first todo",
+      date: "2024-12-29T16:00:00.000Z",
+      isEdited: false,
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Second todo",
+      content: "This is the second todo",
+      date: "2024-12-29T12:00:00.000Z",
+      isEdited: false,
+      completed: false,
+    },
+    {
+      id: "3",
+      title: "Third todo",
+      content: "This is the third todo",
+      date: "2024-12-22T19:00:00.000Z",
+      isEdited: false,
+      completed: false,
+    },
+    {
+      id: "8",
+      title: "Eight todo",
+      content: "This is the third todo",
+      date: "2024-12-27T19:00:00.000Z",
+      isEdited: false,
+      completed: false,
+    },
+  ],
+  completed: [
+    {
+      id: "4",
+      title: "Fourth todo",
+      content: "This is the fourth todo",
+      date: "2024-09-04T00:00:00.000Z",
+      isEdited: false,
+      completed: true,
+    },
+    {
+      id: "5",
+      title: "Fifth todo",
+      content: "This is the fifth todo",
+      date: "2024-09-05T00:00:00.000Z",
+      isEdited: false,
+      completed: true,
+    },
+    {
+      id: "6",
+      title: "Sixth todo",
+      content: "This is the sixth todo",
+      date: "2024-09-06T00:00:00.000Z",
+      isEdited: false,
+      completed: true,
+    },
+  ],
 };
 
 const todosSlice = createSlice({
@@ -81,19 +139,11 @@ const todosSlice = createSlice({
         (todo) => todo.id !== action.payload
       );
     },
-    deleteAllCompleted: (state) => {
-      state.completed = [];
-    },
   },
 });
 
-export const {
-  addTodo,
-  editTodo,
-  toggleCompleteTodoStatus,
-  deleteTodo,
-  deleteAllCompleted,
-} = todosSlice.actions;
+export const { addTodo, editTodo, toggleCompleteTodoStatus, deleteTodo } =
+  todosSlice.actions;
 
 export const selectTodos = (state: RootState) => state.todos.todos;
 export const selectCompleted = (state: RootState) => state.todos.completed;
