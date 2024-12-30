eb5ad7d0d4c816a63eac6b85ecaeb43c8af58f7b
bilalseen <bilalsen2010@gmail.com>
2024-12-30 21:35:23 +0300

    feat: clear initial todos and completed todos state in todoSlice

--- src/redux/todoSlice.tsx
+++ src/redux/todoSlice.tsx
@@ -23,66 +23,8 @@ interface TodosState {
 }
 
 const initialState: TodosState = {
-  todos: [
-    {
-      id: "1",
-      title: "First todo",
-      content: "This is the first todo",
-      date: "2024-12-29T16:00:00.000Z",
-      isEdited: false,
-      completed: false,
-    },
-    {
-      id: "2",
-      title: "Second todo",
-      content: "This is the second todo",
-      date: "2024-12-29T12:00:00.000Z",
-      isEdited: false,
-      completed: false,
-    },
-    {
-      id: "3",
-      title: "Third todo",
-      content: "This is the third todo",
-      date: "2024-12-22T19:00:00.000Z",
-      isEdited: false,
-      completed: false,
-    },
-    {
-      id: "8",
-      title: "Eight todo",
-      content: "This is the third todo",
-      date: "2024-12-27T19:00:00.000Z",
-      isEdited: false,
-      completed: false,
-    },
-  ],
-  completed: [
-    {
-      id: "4",
-      title: "Fourth todo",
-      content: "This is the fourth todo",
-      date: "2024-09-04T00:00:00.000Z",
-      isEdited: false,
-      completed: true,
-    },
-    {
-      id: "5",
-      title: "Fifth todo",
-      content: "This is the fifth todo",
-      date: "2024-09-05T00:00:00.000Z",
-      isEdited: false,
-      completed: true,
-    },
-    {
-      id: "6",
-      title: "Sixth todo",
-      content: "This is the sixth todo",
-      date: "2024-09-06T00:00:00.000Z",
-      isEdited: false,
-      completed: true,
-    },
-  ],
+  todos: [],
+  completed: [],
 };
 
 const todosSlice = createSlice({
