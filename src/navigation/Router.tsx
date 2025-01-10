81ee23f8e86370011b8480ae41c8ac4c58bbe93c
bilalseen <bilalsen2010@gmail.com>
2025-01-10 14:46:31 +0300

    feat: add Splash screen to navigation stack

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -6,6 +6,7 @@ import store from "../redux/store";
 import NoteDetail from "../screens/notes/NoteDetail";
 import Toast from "react-native-toast-message";
 import TodoDetail from "../screens/todo/TodoDetail";
+import Splash from "../screens/Splash";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +19,7 @@ const Router = () => {
             headerShown: false,
           }}
         >
+          <Stack.Screen name="Splash" component={Splash} />
           <Stack.Screen name="BottomNavigation" component={BottomNavigation} />
           <Stack.Screen name="NoteDetail" component={NoteDetail} />
           <Stack.Screen name="TodoDetail" component={TodoDetail} />
